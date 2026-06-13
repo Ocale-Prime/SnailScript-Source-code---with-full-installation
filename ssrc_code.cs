e896ef296d8626b89822debffd27b8a2f36804e7
Ocale-Prime <hemkmishra@gmail.com>
2026-06-13 15:09:16 +0200

    Add func1 class with Main_argument method
    Initial implementation of func1 class with Main_argument method.

---
+++ ssrc_code.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Formats.Asn1;
+
+class func1
+{
+    private static readonly void Main_argument(char[] args)
+    {
+        var func12 = new function();
+        func12.readonly32();
+
+        Console.WriteLine("Function and args: " + func12 + " args: " + args);
+    }
+
+  // More code will be added, and updated, for now this is the function readonly argument
+}
