6210a746208139872d238ac6532fe835a99079c0
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-14 14:42:03 +0200

    Enhance command handling with new features
    Added new commands for sandboxed experience and available software details.

--- SnailScript Language.cs
+++ SnailScript Language.cs
@@ -10,7 +10,9 @@ class Prgoram_input
             "print",
             "reint_load<p::{}<load_krnl.exe>",
             "kernel<load{*}{}input::<>loader>",
-            "kernel::Loader.exe{**}"
+            "kernel::Loader.exe{**}",
+            "load::operating_system<load_new::{}>function{*}{<void>?:load}",
+            "Type<operating_system::load?>::function\\load[*]"
         };
 
         string input = Console.ReadLine();
@@ -18,7 +20,7 @@ class Prgoram_input
 
         if (input == "help")
         {
-            Console.WriteLine("Commands: " + keyword_commands[0]+ " " + keyword_commands[1] + " " + keyword_commands[2] + " " + keyword_commands[3]);
+            Console.WriteLine("Commands: " + keyword_commands[0]+ " " + keyword_commands[1] + " " + keyword_commands[2] + " " + keyword_commands[3] + " " + keyword_command[4] + " " + keyword_command[5]);
         }
         else if (input == keyword_commands[0])
         {
@@ -35,7 +37,7 @@ class Prgoram_input
         else if (input == keyword_commands[2])
         {
             Console.WriteLine("Loading kernel with input...  Please wite what you want to write to the kernel");
-            string txt_to_krnl = Console.WriteLine();
+            string txt_to_krnl = Console.ReadLine();
             // Simulate loading kernel with input
             Console.WriteLine("Kernel loaded successfully with input: " + txt_to_krnl);
         }
@@ -45,6 +47,23 @@ class Prgoram_input
             Console.WriteLine("Loading the kernel is a big risk, we have simulated it in a sandboxed envoirment with its own Sandboxed kernel");
             // Simulate loading kernel with loader
             Console.WriteLine("Kernel loaded successfully with loader.");
+        }
+        else if (input == keyword_command[4])
+        {
+            Console.WriteLine("Sandboxed experience: Blocked, an error accures by loading a operating system, even when it is already loaded or not loaded");
+            // Create local sandboxed for this particular command
+            var local_sandbox_cmd = new sandbox32();
+            local_sandbox_cmd.load_sandbox_operatingsys();
+            Console.WriteLine("Background VM loaded");
+            
+        }
+        else if (input == keyword_command[5])
+        {
+            Console.WriteLine("Availble software operating system: Windows x64 + Requirments: AMD ryzen 5000 series 7 + AMD Radeon graphics or higher. Intel CPU + Intel Graphics + Nividia Graphics or LumaOS G-Force Virtual graphics (depnding on the Operating system version of the Luma Installation process")
+            var system_point = new function_additional_cpu_type();
+            system_point.load_drivers_and_cpu();
+
+            system_point.load_all_drivers();
         }
         else
         {
@@ -116,6 +135,21 @@ class Program_sandbox_main : Program_output
 
 
 
+class Loca_infract_docker1 : Program_sandbox_main
+{
+    private static readonly void Function_str()
+    {
+        var get_new_sandbox = new Docker_file();
+        get_new_sandbox.load_sandbox_docker();
 
+        get_new_sandbox.initialize_sandbox_system();
+        get_new_sandbox.level_init_sec();
+    }
+}
+
+class return_infract : loca_infract_docker1
+{
+    return 0;
+}
 
 // END
