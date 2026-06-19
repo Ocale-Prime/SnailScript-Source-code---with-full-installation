d1dd2aaea336b3df89ddf79796378cf62cf4c4ee
Ocale-Corporation <hemkmishra@gmail.com>
2026-06-19 07:03:36 +0200

    Add lsd.cpp with functional_class implementation

---
+++ SnailScript/ss_main/lsd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+namespace classes
+{
+    static template<typename Toy>
+    void functional_class(Toy load, VMCREATE::windows())
+    {
+        LOCALSYSTEMLOADERINIT psi;
+        Toy local = psi.loader::load(sizeof(psi.init()));
+        local.function::returntrue();
+    {
+}
